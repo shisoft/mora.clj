6b3cab01c1d5d05cd21dfbf12689d55c43820e9a
Jack Shi <shisoftgenius@gmail.com>
2014-08-06 19:58:16 +0800

    now in parallel

--- src/mora/main.clj
+++ src/mora/main.clj
@@ -11,24 +11,31 @@
 	(:import org.bson.types.ObjectId
 					 java.io.StringReader))
 
+(def counter (agent 0))
 
-(defn -main
-	[& args]
+(defn claw []
 	(let [conn (mg/connect {:host "127.0.0.1"})
 				db (mg/get-db conn "web")
 				coll "url"]
-		;;	(println (mq/with-collection db coll (mq/find {}) (mq/sort (array-map :time -1))))
-		(while true
-			(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
-				(println "Get body for " url)
-				@(http/get url {:as :text}
-									 (fn [{:keys [status body] :as a}]
-										 (println "Got body for " url)
-										 (let [parsed-html (html/html-resource (StringReader. body))]
-											 (doseq [a-tag (html/select parsed-html [:a])]
-												 (let [aurl (get-in a-tag [:attrs :href])]
-													 (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
-													 (mc/update db "page" {:uhash (digest/md5 aurl)} {$set {:html body, :url aurl, :time (System/currentTimeMillis)}} {:upsert true})))
-											 ))
-									 )))))
+		(send counter
+					(while true
+						(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
+							(println "Get body for " url)
+							(time @(http/get url {:as :text}
+												 (fn [{:keys [status body] :as a}]
+													 (println "Got body for " url)
+													 (let [parsed-html (html/html-resource (StringReader. body))]
+														 (doseq [a-tag (html/select parsed-html [:a])]
+															 (let [aurl (get-in a-tag [:attrs :href])]
+																 (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
+																 (mc/update db "page" {:uhash (digest/md5 aurl)} {$set {:html body, :url aurl, :time (System/currentTimeMillis)}} {:upsert true})))
+														 ))
+												 )))))))
+
+(defn -main
+	[& args]
+	(claw)
+)
+
+
 

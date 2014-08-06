728a701798fa7d6f54d6db8e73e67bc8c285adc3
Jack Shi <shisoftgenius@gmail.com>
2014-08-07 01:10:25 +0800

    That was really stupid, fixed dup pages
    the upsert page clause was go with insert page urls

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
 									[enlive "1.1.5"]
 									[digest "1.4.4"]
 									[com.syncthemall/boilerpipe "1.2.2"]
-									[org.jsoup/jsoup "1.7.2"]
+									[org.clojars.clizzin/jsoup "1.5.1"]
 									;[io.curtis/boilerpipe-clj "0.3.0"]
 								]
   :main ^:skip-aot mora.main
--- src/mora/main.clj
+++ src/mora/main.clj
@@ -1,5 +1,5 @@
 (ns mora.main
-  (:gen-class)
+	(:gen-class)
 	(require [org.httpkit.client :as http]
 					 [monger.core :as mg]
 					 [monger.collection :as mc]
@@ -11,13 +11,13 @@
 	(:import org.bson.types.ObjectId
 					 java.io.StringReader
 					 de.l3s.boilerpipe.extractors.ArticleExtractor
-					 org.jsoup
+					 org.jsoup.Jsoup
 					 ))
 
 (defn strip-html-tags
 	"Function strips HTML tags from string."
 	[s]
-	(.text (org.jsoup/parse s)))
+	(.text (org.jsoup.Jsoup/parse s)))
 
 (def counter (agent 0))
 
@@ -30,25 +30,27 @@
 						(while true
 							(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
 								;(println "Get body for " url)
-								(time @(http/get url {:as :text}
-																 (fn [{:keys [body] :as a}]
-																	 ;(println "Got body for " url)
-																	 (let [parsed-html (html/html-resource (StringReader. body))]
-																		 (doseq [a-tag (html/select parsed-html [:a])]
-																			 (let [aurl (get-in a-tag [:attrs :href]), plain (strip-html-tags body), text (.getText extractor body), isa (not (clojure.string/blank? text))]
-																				 (if isa (do
-																									 (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
-																									 (mc/update db "page" {:uhash (digest/md5 aurl)} {$set {:html body, :text text, :isa (empty? text), :plain plain, :url aurl, :time (System/currentTimeMillis)}} {:upsert true})
-																									 ))
-																				 )))
-																	 ))
-											))))
+								(http/get url {:as :text}
+													(fn [{:keys [body error] :as a}]
+														;(println "Got body for " url)
+														(if (not error)
+															(time
+																(let [parsed-html (html/html-resource (StringReader. body))]
+																	(doseq [a-tag (html/select parsed-html [:a])]
+																		(let [aurl (get-in a-tag [:attrs :href])]
+																			(mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
+																			))
+																	(let [plain (strip-html-tags body), text (.getText extractor body), isa (not (clojure.string/blank? plain))]
+																							 (if isa (mc/update db "page" {:uhash (digest/md5 url)} {$set {:html body, :text text, :isa (empty? text), :plain plain, :url url, :time (System/currentTimeMillis)}} {:upsert true})))))
+															(println "Failed" error))
+														))
+								)))
 					)))
 
 (defn -main
 	[& args]
 	(claw)
-)
+	)
 
 
 

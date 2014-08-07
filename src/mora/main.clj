ffc42894f8f6afd0989618cf0ddf29c7dbdc41a3
Jack Shi <shisoftgenius@gmail.com>
2014-08-07 11:17:57 +0800

    Fixed some exception

--- src/mora/main.clj
+++ src/mora/main.clj
@@ -21,11 +21,14 @@
 	(.text (org.jsoup.Jsoup/parse s)))
 
 (defn check-abs-link [href base-url]
-	(if (.contains href "://")
-		href
-		(str (StringUtils/join (drop-last (seq (.split (str (if (.equals (last base-url) \/) base-url (str base-url "/")) " ") "/"))) "/")
-				 "/"
-				 (if (.equals (first href) \/) (href rest) href))))
+	(if (empty? href) nil
+										(if (.contains href "://")
+											href
+											(str (StringUtils/join (drop-last (seq (.split (str (if (.equals (last base-url) \/) base-url (str base-url "/")) " ") "/"))) "/")
+													 "/"
+													 (if (.equals (first href) \/)
+														 (rest href)
+														 href)))))
 
 (defn claw []
 	(let [conn (mg/connect {:host "127.0.0.1"})
@@ -35,15 +38,15 @@
 			(while true
 				(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
 					;(println "Get body for " url)
-					(http/get url {:as :text}
-										 (fn [{:keys [body error] :as a}]
+					(http/get url {:as :auto}
+										 (fn [{:keys [body error status] :as a}]
 											 (println url)
-											 (if (not error)
+											 (if (and (not error) (= status 200) (string? body))
 												 (time
 													 (let [parsed-html (html/html-resource (StringReader. body))]
 														 (doseq [a-tag (html/select parsed-html [:a])]
 															 (let [aurl (check-abs-link (get-in a-tag [:attrs :href]) url)]
-																 (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
+																 (if (not (empty? aurl)) (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)}))
 																 ))
 														 (let [plain (strip-html-tags body), text (.getText extractor body), isa (not (clojure.string/blank? text))]
 															 (if isa (mc/update db "page" {:uhash (digest/md5 url)} {$set {:html body, :text text, :isa (empty? text), :plain plain, :url url, :time (System/currentTimeMillis)}} {:upsert true})))))

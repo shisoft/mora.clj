97e43a5f5808966449a893cc3b5ee241be05d1dc
Jack Shi <shisoftgenius@gmail.com>
2014-08-07 10:33:33 +0800

    async fixs
    MongoDB gets ‘Too many open files’ when there is only seed url in the
    ‘url’ collection

--- src/mora/main.clj
+++ src/mora/main.clj
@@ -20,8 +20,6 @@
 	[s]
 	(.text (org.jsoup.Jsoup/parse s)))
 
-(def counter (agent 0))
-
 (defn check-abs-link [href base-url]
 	(if (.contains href "://")
 		href
@@ -33,27 +31,26 @@
 	(let [conn (mg/connect {:host "127.0.0.1"})
 				db (mg/get-db conn "web")
 				coll "url"]
-		(send counter
-					(let [extractor (.newInstance ArticleExtractor)]
-						(while true
-							(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
-								;(println "Get body for " url)
-								@(http/get url {:as :text}
-													(fn [{:keys [body error] :as a}]
-														;(println "Got body for " url)
-														(if (not error)
-															(time
-																(let [parsed-html (html/html-resource (StringReader. body))]
-																	(doseq [a-tag (html/select parsed-html [:a])]
-																		(let [aurl (check-abs-link (get-in a-tag [:attrs :href]) url)]
-																			(mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
-																			))
-																	(let [plain (strip-html-tags body), text (.getText extractor body), isa (not (clojure.string/blank? text))]
-																							 (if isa (mc/update db "page" {:uhash (digest/md5 url)} {$set {:html body, :text text, :isa (empty? text), :plain plain, :url url, :time (System/currentTimeMillis)}} {:upsert true})))))
-															(println "Failed" error))
-														))
-								)))
+		(let [extractor (.newInstance ArticleExtractor)]
+			(while true
+				(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
+					;(println "Get body for " url)
+					(http/get url {:as :text}
+										 (fn [{:keys [body error] :as a}]
+											 (println url)
+											 (if (not error)
+												 (time
+													 (let [parsed-html (html/html-resource (StringReader. body))]
+														 (doseq [a-tag (html/select parsed-html [:a])]
+															 (let [aurl (check-abs-link (get-in a-tag [:attrs :href]) url)]
+																 (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)})
+																 ))
+														 (let [plain (strip-html-tags body), text (.getText extractor body), isa (not (clojure.string/blank? text))]
+															 (if isa (mc/update db "page" {:uhash (digest/md5 url)} {$set {:html body, :text text, :isa (empty? text), :plain plain, :url url, :time (System/currentTimeMillis)}} {:upsert true})))))
+												 (println "Failed" error))
+											 ))
 					)))
+		))
 
 (defn -main
 	[& args]

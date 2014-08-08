fef7955a2da97ae74cfc01e37d9e8617be2be4dd
Jack Shi <shisoftgenius@gmail.com>
2014-08-08 10:53:06 +0800

    Clyde version

--- src/mora/main.clj
+++ src/mora/main.clj
@@ -1,34 +1,59 @@
-(ns mora.main
-	(:gen-class)
-	(require [org.httpkit.client :as http]
-					 [monger.core :as mg]
-					 [monger.collection :as mc]
-					 [monger.query :as mq]
-					 [monger.operators :refer :all]
-					 [net.cgrand.enlive-html :as html]
-					 [digest]
-					 [clojure.pprint])
-	(:import org.bson.types.ObjectId
-					 java.io.StringReader
-					 de.l3s.boilerpipe.extractors.ArticleExtractor
-					 org.jsoup.Jsoup
-					 org.apache.commons.lang.StringUtils
-					 ))
+(ns mora.main (:gen-class)
+							(require [org.httpkit.client :as http]
+											 [monger.core :as mg]
+											 [monger.collection :as mc]
+											 [monger.query :as mq]
+											 [monger.operators :refer :all]
+											 [net.cgrand.enlive-html :as html]
+											 [digest]
+											 [clojure.pprint])
+							(:import org.bson.types.ObjectId
+											 java.io.StringReader
+											 de.l3s.boilerpipe.extractors.ArticleExtractor
+											 org.jsoup.Jsoup
+											 org.apache.commons.lang.StringUtils))
 
 (defn strip-html-tags
 	"Function strips HTML tags from string."
 	[s]
 	(.text (org.jsoup.Jsoup/parse s)))
 
-(defn check-abs-link [href base-url]
-	(if (empty? href) nil
-										(if (.contains href "://")
-											href
-											(str (StringUtils/join (drop-last (seq (.split (str (if (.equals (last base-url) \/) base-url (str base-url "/")) " ") "/"))) "/")
-													 "/"
-													 (if (.equals (first href) \/)
-														 (rest href)
-														 href)))))
+(defn abs-link [href base-url]
+	(if (seq href)
+		(if (.contains href "://")
+			href
+			(let [remove-blank (partial remove clojure.string/blank?)
+						drop-all-upwards (partial drop-while (partial = ".."))
+
+						base-url-blocks*
+						(remove-blank
+							((if (= (last base-url) \/) identity drop-last)
+							 (clojure.string/split base-url #"/")))
+						base-url-blocks (reverse (drop-all-upwards (reverse base-url-blocks*)))
+						base-url-blocks (drop-last (- (count base-url-blocks*) (count base-url-blocks)) base-url-blocks)
+
+						href-blocks* (remove-blank (clojure.string/split href #"/"))
+						href-blocks (drop-all-upwards href-blocks*)
+						base-url-blocks (drop-last (- (count href-blocks*) (count href-blocks)) base-url-blocks)]
+
+				(clojure.string/join "/" (concat base-url-blocks (drop-while (partial = ".") href-blocks)))))))
+
+(defn check-and-insert-linked-urls! [url body db coll]
+	(doseq [a-tag (html/select (html/html-resource (StringReader. body))
+														 [:a :attrs :href]) ]
+		(when-let [aurl (seq (abs-link a-tag url))]
+			(try
+				(mc/insert db coll {:url (apply str aurl), :time (System/currentTimeMillis)})
+				(catch Exception e (println (.getMessage e)))))))
+
+(defn strip-text-and-insert! [url body db coll extractor]
+	(let [plain (strip-html-tags body)
+				text (.getText extractor body)]
+		(when-not (clojure.string/blank? text)
+			(mc/update db "page"
+								 {:uhash (digest/md5 url)}
+								 {$set {:html body, :text text, :isa (empty? text), :plain plain, :url url, :time (System/currentTimeMillis)}}
+								 {:upsert true}))))
 
 (defn claw []
 	(let [conn (mg/connect {:host "127.0.0.1"})
@@ -36,29 +61,17 @@
 				coll "url"]
 		(let [extractor (.newInstance ArticleExtractor)]
 			(while true
-				(let [{url :url} (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)})]
-					;(println "Get body for " url)
-					(http/get url {:as :auto}
-										 (fn [{:keys [body error status] :as a}]
-											 (println url)
-											 (if (and (not error) (= status 200) (string? body))
-												 (time
-													 (let [parsed-html (html/html-resource (StringReader. body))]
-														 (doseq [a-tag (html/select parsed-html [:a])]
-															 (let [aurl (check-abs-link (get-in a-tag [:attrs :href]) url)]
-																 (if (not (empty? aurl)) (mc/insert db coll {:url aurl, :time (System/currentTimeMillis)}))
-																 ))
-														 (let [plain (strip-html-tags body), text (.getText extractor body), isa (not (clojure.string/blank? text))]
-															 (if isa (mc/update db "page" {:uhash (digest/md5 url)} {$set {:html body, :text text, :isa (empty? text), :plain plain, :url url, :time (System/currentTimeMillis)}} {:upsert true})))))
-												 (println "Failed" error))
-											 ))
-					)))
-		))
+				(let [url (:url (mc/find-and-modify db coll {} {$set {:time (System/currentTimeMillis)}} {:sort (array-map :time 1, :_id 1)}))
 
-(defn -main
-	[& args]
-	(claw)
-	)
+							process-page
+							(fn [{:keys [body error status]}]
+								(println url)
+								(if (and (not error) (= status 200) (string? body))
+									(do
+										(check-and-insert-linked-urls! url body db coll)
+										(strip-text-and-insert! url body db coll extractor))
+									(println "Failed" error)))]
+					(http/get url {:as :auto} process-page))))))
 
-
-
+(defn -main [& args]
+	(claw))
